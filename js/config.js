e343f6b97b3adffbadd86203120ec3ee6bcf436f
anupam-tiwari <appleanupam@gmail.com>
2020-06-12 11:54:00 -0700

    third commit

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_3cz68O7SE', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '491q5mjb4fp50vtg6q0jlqutiu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
